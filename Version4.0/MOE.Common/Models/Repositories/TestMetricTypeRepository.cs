7382ae6a63b1966da22cf3ce3591bb65b1a832e1
dlowe <dlowe@utah.gov>
2017-04-18 14:00:51 -0600

    Create a folder for version 4.0

---
+++ Version4.0/MOE.Common/Models/Repositories/TestMetricTypeRepository.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOE.Common.Models.Repositories
+{
+    public class TestMetricTypeRepository : IMetricTypeRepository
+    {
+        Models.SPM db = new SPM();
+
+        public List<Models.MetricType> GetAllMetrics()
+        {
+            List<Models.MetricType> results = new List<MetricType>();
+            MetricType purduePhase = new MetricType();
+            purduePhase.MetricID = 1;
+            purduePhase.ChartName = "Purdue Phase Termination";
+            purduePhase.ShowOnWebsite = true;
+            purduePhase.DetectionTypeID = 1;
+            results.Add(purduePhase);
+
+            MetricType split = new MetricType();
+            split.MetricID = 2;
+            split.ChartName = "Split Monitor";
+            split.ShowOnWebsite = true;
+            split.DetectionTypeID = 1;
+            results.Add(split);
+
+            MetricType ped = new MetricType();
+            ped.MetricID = 3;
+            ped.ChartName = "Pedestrian Delay";
+            ped.ShowOnWebsite = true;
+            ped.DetectionTypeID = 1;
+            results.Add(ped);
+
+            MetricType preempt = new MetricType();
+            preempt.MetricID = 4;
+            preempt.ChartName = "Preemption Details";
+            preempt.ShowOnWebsite = true;
+            preempt.DetectionTypeID = 1;
+            results.Add(preempt);
+
+            MetricType tmc = new MetricType();
+            tmc.MetricID = 5;
+            tmc.ChartName = "Turning Movement Counts";
+            tmc.ShowOnWebsite = true;
+            tmc.DetectionTypeID = 4;
+            results.Add(tmc);
+
+            MetricType pcd = new MetricType();
+            pcd.MetricID = 6;
+            pcd.ChartName = "Purdue Coordination Diagram";
+            pcd.ShowOnWebsite = true;
+            pcd.DetectionTypeID = 2;
+            results.Add(pcd);
+
+            MetricType approach = new MetricType();
+            approach.MetricID = 7;
+            approach.ChartName = "Approach Volume";
+            approach.ShowOnWebsite = true;
+            approach.DetectionTypeID = 2;
+            results.Add(approach);
+
+            MetricType delay = new MetricType();
+            delay.MetricID = 8;
+            delay.ChartName = "Approach Delay";
+            delay.ShowOnWebsite = true;
+            delay.DetectionTypeID = 2;
+            results.Add(delay);
+
+            MetricType aor = new MetricType();
+            aor.MetricID = 9;
+            aor.ChartName = "Arrivals on Red";
+            aor.ShowOnWebsite = true;
+            aor.DetectionTypeID = 2;
+            results.Add(aor);
+
+            MetricType speed = new MetricType();
+            speed.MetricID = 10;
+            speed.ChartName = "Approach Speed";
+            speed.ShowOnWebsite = true;
+            speed.DetectionTypeID = 3;
+            results.Add(speed);
+
+            MetricType yra = new MetricType();
+            yra.MetricID = 11;
+            yra.ChartName = "Yellow and Red Actuations";
+            yra.ShowOnWebsite = true;
+            yra.DetectionTypeID = 5;
+            results.Add(yra);
+
+
+            MetricType splitFail = new MetricType();
+            splitFail.MetricID = 12;
+            splitFail.ChartName = "Purdue Split Failure";
+            splitFail.ShowOnWebsite = true;
+            splitFail.DetectionTypeID = 6;
+            results.Add(splitFail);
+
+            return results;
+        }
+
+        public List<MetricType> GetAllToDisplayMetrics()
+        {
+            List<Models.MetricType> results = new List<MetricType>();
+            MetricType purduePhase = new MetricType();
+            purduePhase.MetricID = 1;
+            purduePhase.ChartName = "Purdue Phase Termination";
+            purduePhase.ShowOnWebsite = true;
+            purduePhase.DetectionTypeID = 1;
+            results.Add(purduePhase);
+
+            MetricType split = new MetricType();
+            split.MetricID = 2;
+            split.ChartName = "Split Monitor";
+            split.ShowOnWebsite = true;
+            split.DetectionTypeID = 1;
+            results.Add(split);
+
+            MetricType ped = new MetricType();
+            ped.MetricID = 3;
+            ped.ChartName = "Pedestrian Delay";
+            ped.ShowOnWebsite = true;
+            ped.DetectionTypeID = 1;
+            results.Add(ped);
+
+            MetricType preempt = new MetricType();
+            preempt.MetricID = 4;
+            preempt.ChartName = "Preemption Details";
+            preempt.ShowOnWebsite = true;
+            preempt.DetectionTypeID = 1;
+            results.Add(preempt);
+
+            MetricType tmc = new MetricType();
+            tmc.MetricID = 5;
+            tmc.ChartName = "Turning Movement Counts";
+            tmc.ShowOnWebsite = true;
+            tmc.DetectionTypeID = 4;
+            results.Add(tmc);
+
+            MetricType pcd = new MetricType();
+            pcd.MetricID = 6;
+            pcd.ChartName = "Purdue Coordination Diagram";
+            pcd.ShowOnWebsite = true;
+            pcd.DetectionTypeID = 2;
+            results.Add(pcd);
+
+            MetricType approach = new MetricType();
+            approach.MetricID = 7;
+            approach.ChartName = "Approach Volume";
+            approach.ShowOnWebsite = true;
+            approach.DetectionTypeID = 2;
+            results.Add(approach);
+
+            MetricType delay = new MetricType();
+            delay.MetricID = 8;
+            delay.ChartName = "Approach Delay";
+            delay.ShowOnWebsite = true;
+            delay.DetectionTypeID = 2;
+            results.Add(delay);
+
+            MetricType aor = new MetricType();
+            aor.MetricID = 9;
+            aor.ChartName = "Arrivals on Red";
+            aor.ShowOnWebsite = true;
+            aor.DetectionTypeID = 2;
+            results.Add(aor);
+
+            MetricType speed = new MetricType();
+            speed.MetricID = 10;
+            speed.ChartName = "Approach Speed";
+            speed.ShowOnWebsite = true;
+            speed.DetectionTypeID = 3;
+            results.Add(speed);
+
+            MetricType yra = new MetricType();
+            yra.MetricID = 11;
+            yra.ChartName = "Yellow and Red Actuations";
+            yra.ShowOnWebsite = true;
+            yra.DetectionTypeID = 5;
+            results.Add(yra);
+
+
+            MetricType splitFail = new MetricType();
+            splitFail.MetricID = 12;
+            splitFail.ChartName = "Purdue Split Failure";
+            splitFail.ShowOnWebsite = true;
+            splitFail.DetectionTypeID = 6;
+            results.Add(splitFail);
+            return results;
+        }
+
+        public List<MetricType> GetBasicMetrics()
+        {
+            List<Models.MetricType> results = new List<MetricType>();
+            MetricType purduePhase = new MetricType();
+            purduePhase.MetricID = 1;
+            purduePhase.ChartName = "Purdue Phase Termination";
+            purduePhase.ShowOnWebsite = true;
+            purduePhase.DetectionTypeID = 1;
+            results.Add(purduePhase);
+
+            MetricType split = new MetricType();
+            split.MetricID = 2;
+            split.ChartName = "Split Monitor";
+            split.ShowOnWebsite = true;
+            split.DetectionTypeID = 1;
+            results.Add(split);
+
+            MetricType ped = new MetricType();
+            ped.MetricID = 3;
+            ped.ChartName = "Pedestrian Delay";
+            ped.ShowOnWebsite = true;
+            ped.DetectionTypeID = 1;
+            results.Add(ped);
+
+            MetricType preempt = new MetricType();
+            preempt.MetricID = 4;
+            preempt.ChartName = "Preemption Details";
+            preempt.ShowOnWebsite = true;
+            preempt.DetectionTypeID = 1;
+            results.Add(preempt);
+
+            return results;
+        }
+
+        public List<MetricType> GetMetricsByIDs(List<int> metricIDs)
+        {
+            return db.MetricTypes.Where(x => metricIDs.Contains(x.MetricID)).ToList();
+        }
+        public MetricType GetMetricsByID(int metricID)
+        {
+            return db.MetricTypes.Find(metricID);
+        }
+
+        public List<Models.MetricType> GetMetricTypesByMetricComment(Models.MetricComment metricComment)
+        {
+            return db.MetricTypes.Where(x => metricComment.MetricTypeIDs.Contains(x.MetricID)).ToList();
+        }
+    }
+}
---
+++ Version4.0/SPM/Controllers/ApproachRouteDetailsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MOE.Common.Models;
+
+namespace SPM.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class ApproachRouteDetailsController : Controller
+    {
+        private MOE.Common.Models.SPM db = new MOE.Common.Models.SPM();
+
+        // GET: ApproachRouteDetails
+        public ActionResult Index(int id)
+        {
+            var approachRouteDetails = db.ApproachRouteDetails
+                .Include(a => a.Approach)
+                .Include(a => a.ApproachRoute)
+                .Where(a=> a.ApproachRouteId == id);           
+            return View(approachRouteDetails.ToList());
+        }
+
+        // GET: ApproachRouteDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApproachRouteDetail approachRouteDetail = db.ApproachRouteDetails.Find(id);
+            if (approachRouteDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(approachRouteDetail);
+        }
+
+        // GET: ApproachRouteDetails/Create
+        public ActionResult Create(int id)
+        {
+            ViewBag.ApproachID = new SelectList(db.Approaches, "ApproachID", "ApproachRouteDescription");
+            ViewBag.ApproachRouteId = new SelectList(db.ApproachRoutes, "ApproachRouteId", "RouteName", id);
+            return View();
+        }
+
+        // POST: ApproachRouteDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RouteDetailID,ApproachRouteId,ApproachOrder,ApproachID")] ApproachRouteDetail approachRouteDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ApproachRouteDetails.Add(approachRouteDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index", new { id = approachRouteDetail.ApproachRouteId });
+            }
+
+            ViewBag.ApproachID = new SelectList(db.Approaches, "ApproachID", "ApproachRouteDescription", approachRouteDetail.ApproachID);
+            ViewBag.ApproachRouteId = new SelectList(db.ApproachRoutes, "ApproachRouteId", "RouteName", approachRouteDetail.ApproachRouteId);
+            return View(approachRouteDetail);
+        }
+
+        // GET: ApproachRouteDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApproachRouteDetail approachRouteDetail = db.ApproachRouteDetails.Find(id);
+            if (approachRouteDetail == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ApproachID = new SelectList(db.Approaches, "ApproachID", "ApproachRouteDescription", approachRouteDetail.ApproachID);
+            ViewBag.ApproachRouteId = new SelectList(db.ApproachRoutes, "ApproachRouteId", "RouteName", approachRouteDetail.ApproachRouteId);
+            return View(approachRouteDetail);
+        }
+
+        // POST: ApproachRouteDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RouteDetailID,ApproachRouteId,ApproachOrder,ApproachID")] ApproachRouteDetail approachRouteDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(approachRouteDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ApproachID = new SelectList(db.Approaches, "ApproachID", "ApproachRouteDescription", approachRouteDetail.ApproachID);
+            ViewBag.ApproachRouteId = new SelectList(db.ApproachRoutes, "ApproachRouteId", "RouteName", approachRouteDetail.ApproachRouteId);
+            return View(approachRouteDetail);
+        }
+
+        // GET: ApproachRouteDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApproachRouteDetail approachRouteDetail = db.ApproachRouteDetails.Find(id);
+            if (approachRouteDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(approachRouteDetail);
+        }
+
+        // POST: ApproachRouteDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ApproachRouteDetail approachRouteDetail = db.ApproachRouteDetails.Find(id);
+            db.ApproachRouteDetails.Remove(approachRouteDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
